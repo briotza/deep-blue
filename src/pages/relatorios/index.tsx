0045bba9da93e729f689bb0e3ab6284fdd6bee69
briotza <cianomeliunas@gmail.com>
2024-11-26 15:04:25 -0300

    feat: página de relatórios

---
+++ src/pages/relatorios/index.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Sidebar from "../../components/dashboard/sidebar";
+import Topbar from "../../components/dashboard/topbar";
+import ListaIncidentes from "../../components/incidentes/listaIncidentes";
+import Custos from "../../components/relatorios/custos";
+
+export default function Relatorios() {
+
+    return(
+        <div className="flex flex-row h-screen">
+            <Sidebar />
+            <div className="flex flex-col w-[100%]">
+                <Topbar />
+                <Custos />
+            </div>
+        </div>
+    )
+}
