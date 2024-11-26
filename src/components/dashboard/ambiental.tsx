ae3bd9b31bdb20616fc340e65243fa901ad58286
briotza <cianomeliunas@gmail.com>
2024-11-26 15:01:33 -0300

    refactor: atualização nos dados exibidos no gráfico

--- src/components/dashboard/ambiental.tsx
+++ src/components/dashboard/ambiental.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import { Acidente } from '../../types/incidentes.types';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const Ambiental: React.FC = () => {
+const Ambiental: React.FC<{ incidentes: Acidente[] }> = ({ incidentes }) => {
+
+  const incidentesAmbientais = incidentes.filter((incidente) => incidente.tipo === 'Ambiental');
+
+
+  const calcularIncidentesPorMes = (dados: Acidente[]) => {
+    const meses = Array(12).fill(0); 
+    dados.forEach((incidente) => {
+      const mes = new Date(incidente.data).getMonth(); 
+      meses[mes] += 1;
+    });
+    return meses;
+  };
+
+  const dadosPorMes = calcularIncidentesPorMes(incidentesAmbientais);
+
   const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    labels: [
+      "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ],
     datasets: [
       {
-        label: 'Sales',
-        data: [120, 190, 300, 500, 200, 300],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
+        label: "Incidentes Ambientais",
+        data: dadosPorMes,
+        backgroundColor: "rgba(75, 192, 192, 0.2)",
+        borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1,
       },
     ],
@@ -22,11 +40,11 @@ const Ambiental: React.FC = () => {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const,
+        position: "top" as const,
       },
       title: {
         display: true,
-        text: 'Incidentes Ambientais',
+        text: "Incidentes Ambientais por Mês",
       },
     },
   };
